be66f735a3785a14f0e3a240406f90f6cb00deb9
Construction Supervision System <construction-supervision@example.com>
2025-12-07 11:13:25 +0200

    Add setup.py for Python detection

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='construction-supervision',
+    version='1.0.0',
+    description='Construction supervision application',
+    packages=[],
+)
